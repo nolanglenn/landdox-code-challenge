66132879d897b96053e43f869aa336a22f5031fa
unknown <nolangglenn@gmail.com>
2020-09-10 11:40:28 -0500

    Updates

---
+++ precache-manifest.319aa84bb3198ed480f91e292b6c978f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ff6eac93ce697fd54121d390ef9551",
+    "url": "/landdox-code-challenge/index.html"
+  },
+  {
+    "revision": "5751c9802c33bff93c19",
+    "url": "/landdox-code-challenge/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "637c0083c957744ac58c",
+    "url": "/landdox-code-challenge/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "5751c9802c33bff93c19",
+    "url": "/landdox-code-challenge/static/js/2.ef63f5f6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/landdox-code-challenge/static/js/2.ef63f5f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "637c0083c957744ac58c",
+    "url": "/landdox-code-challenge/static/js/main.060a2889.chunk.js"
+  },
+  {
+    "revision": "4c8b99672d6a293df179",
+    "url": "/landdox-code-challenge/static/js/runtime-main.e6216a5d.js"
+  }
+]);
